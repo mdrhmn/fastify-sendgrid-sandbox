691bc1cf19d5e9c5fbc733ebe55fbce5389da79b
Abdulmanab <Rahiman.Abdulmanab@Dell.com>
2022-08-01 21:09:25 +0800

    Initial project files commit

---
+++ src/sendEmail.js
@@ -0,0 +1,49 @@
+const fastify = require('fastify')()
+
+fastify.register(require('fastify-sendgrid'), {
+    apiKey: process.env.SENDGRID_API_KEY
+})
+
+fastify.get('/sendmail/:email', (req, reply, next) => {
+    let { sendgrid } = fastify
+    let recipient = req.params.email
+
+    console.log("SENDING EMAIL")
+
+    const msg = {
+        to: recipient,
+        from: 'rahiman.abdulmanab@dell.com', // Use the email address or domain you verified above
+        subject: 'Sending with Twilio SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    //   //ES6F
+    //   sendgrid
+    //     .send(msg)
+    //     .then(() => {}, error => {
+    //       console.error(error);
+
+    //       if (error.response) {
+    //         console.error(error.response.body)
+    //       }
+    //     });
+    //ES8
+    (async () => {
+        try {
+            await sendgrid.send(msg);
+        } catch (error) {
+            console.error(error);
+
+            if (error.response) {
+                console.error(error.response.body)
+            }
+        }
+    })();
+
+    console.log("EMAIL SENT")
+})
+
+fastify.listen(3000, err => {
+    if (err) throw err
+    console.log(`server listening on ${fastify.server.address().port}`)
+})
